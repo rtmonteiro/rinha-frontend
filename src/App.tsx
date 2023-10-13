bac761f2ee6eea396a906e006a06619187d574e1
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2023-10-13 02:34:24 -0300

    feat: apresenta o json
    no pokedex já gargala

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import './App.css'
+import {createSignal, For, Match, Show, Switch} from "solid-js";
 
 function App() {
-    const input = document.querySelector('input[type="file"]');
-
-    const fileReader = new FileReader()
-    let object = null
+    let [object, setObject] =
+        createSignal(null as any);
+    let [error, setError] =
+        createSignal(false);
 
     async function parseJsonFile(file: Blob) {
+
         return new Promise((resolve, reject) => {
-            fileReader.onload = (event: Event) => resolve(JSON.parse(event.target!.result))
-            fileReader.onerror = error => reject(error)
-            fileReader.readAsText(file)
+            const fileReader = new FileReader();
+            fileReader.onloadend = (event: any) => resolve(event.target.result)
+            fileReader.onerror = (error: any) => reject(error)
+            fileReader.readAsArrayBuffer(file)
         })
     }
 
-    input!.addEventListener('change', async function (e) {
+    async function handleUploadFile(e: any) {
         const file = e?.target?.files[0]
-        object = await parseJsonFile(file)
+        setObject(await parseJsonFile(file)
+            .then((result) => {
+                try {
+                    const json = JSON.parse(new TextDecoder().decode(result as ArrayBuffer))
+                    console.log(json)
+                    setError(false)
+                    return json;
+                } catch (e) {
+                    if (e instanceof SyntaxError) {
+                        console.log("Invalid JSON file")
+                        setError(true)
+                    }
+                    return null;
+                }
+            }))
+    }
 
-        window.open('/json.html', '_blank')
-    })
-
+    const Obj = ({object}: { object: any }) => <For each={Object.keys(object)}>
+        {(key) => (
+            <div class="node">
+                <Switch
+                    fallback={<p class="value">{key}:{object[key].toString()}</p>}>
+                    <Match when={typeof object[key] === "object" && object[key] !== null}>
+                        <div class="key">{key}</div>
+                        <Obj object={object[key]}/>
+                    </Match>
+                    <Match when={object[key] === null}>
+                        <p class="value">{key}: null</p>
+                    </Match>
+                </Switch>
+            </div>
+        )}
+    </For>;
     return (
         <>
             <div class="container">
@@ -28,9 +59,20 @@ function App() {
                 <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
                 <div class="input">
                     <label for="up_file">Load JSON</label>
-                    <input type="file" name="up_file" id="up_file" accept=".json"/>
+                    <input onChange={handleUploadFile}
+                           type="file"
+                           name="up_file"
+                           id="up_file"
+                           accept=".json"/>
                 </div>
-                <p class="error">Invalid file. Please load a valid JSON file.</p>
+                <Show when={error()}>
+                    <p class="error">Invalid file. Please load a valid JSON file.</p>
+                </Show>
+                <Show when={object() !== null}>
+                    <div class="tree">
+                        {<Obj object={object()}/>}
+                    </div>
+                </Show>
             </div>
         </>
     )
