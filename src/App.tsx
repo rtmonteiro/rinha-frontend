068fc310c5a2d84d3d3e5fe0b1dc8ae598737cd4
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2023-10-13 17:57:55 -0300

    feat: melhor estilização dos tipos de retorno (array, string, number, bool)
    - até o startwitharray tanka

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
         createSignal(null as any);
     let [error, setError] =
         createSignal(false);
+    let [fileName, setFileName] =
+        createSignal("")
 
     async function parseJsonFile(file: Blob) {
 
@@ -19,6 +21,7 @@ function App() {
 
     async function handleUploadFile(e: any) {
         const file = e?.target?.files[0]
+        setFileName(file.name)
         setObject(await parseJsonFile(file)
             .then((result) => {
                 try {
@@ -36,41 +39,54 @@ function App() {
             }))
     }
 
-    const Obj = ({object}: { object: any }) => <For each={Object.keys(object)}>
-        {(key) => (
-            <div class="node">
-                <Switch
-                    fallback={<p class="value">{key}:{object[key].toString()}</p>}>
-                    <Match when={typeof object[key] === "object" && object[key] !== null}>
-                        <div class="key">{key}</div>
-                        <Obj object={object[key]}/>
-                    </Match>
-                    <Match when={object[key] === null}>
-                        <p class="value">{key}: null</p>
-                    </Match>
-                </Switch>
-            </div>
-        )}
-    </For>;
+    const Obj = ({object}: { object: Record<string, any> }) => (
+        <For each={Object.keys(object)}>
+            {(key) => (
+                <div class="node">
+                    <Switch>
+                        <Match when={typeof object[key] === "object" && object[key] !== null}>
+                            <p class="key" classList={{array: Array.isArray(object[key])}}>{key}:</p>
+                            <Obj object={object[key]}/>
+                        </Match>
+                        <Match when={object[key] === null}>
+                            <p class="value">{key}: null</p>
+                        </Match>
+                        <Match when={typeof object[key] === "string"}>
+                            <p class="value">{key}: "{object[key]}"</p>
+                        </Match>
+                        <Match when={typeof object[key] === "number"}>
+                            <p class="value">{key}: {object[key]}</p>
+                        </Match>
+                        <Match when={typeof object[key] === "boolean"}>
+                            <p class="value">{key}: {object[key] ? "true" : "false"}</p>
+                        </Match>
+                    </Switch>
+                </div>
+            )}
+        </For>);
+
     return (
         <>
             <div class="container">
-                <h1>JSON Tree Viewer</h1>
-                <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
-                <div class="input">
-                    <label for="up_file">Load JSON</label>
-                    <input onChange={handleUploadFile}
-                           type="file"
-                           name="up_file"
-                           id="up_file"
-                           accept=".json"/>
-                </div>
-                <Show when={error()}>
-                    <p class="error">Invalid file. Please load a valid JSON file.</p>
+                <Show when={object() === null}>
+                    <h1>JSON Tree Viewer</h1>
+                    <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
+                    <div class="input">
+                        <label for="up_file">Load JSON</label>
+                        <input onChange={handleUploadFile}
+                               type="file"
+                               name="up_file"
+                               id="up_file"
+                               accept=".json"/>
+                    </div>
+                    <Show when={error()}>
+                        <p class="error">Invalid file. Please load a valid JSON file.</p>
+                    </Show>
                 </Show>
                 <Show when={object() !== null}>
+                    <h2>{fileName()}</h2>
                     <div class="tree">
-                        {<Obj object={object()}/>}
+                        <Obj object={object()}/>
                     </div>
                 </Show>
             </div>
