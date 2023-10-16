d656ea29310f448261cf6a0afc6407e92dfc1849
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2023-10-16 15:49:38 -0300

    fix: worker não funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,45 @@
 import "./App.css";
-import { createSignal, onCleanup, Show } from "solid-js";
+import { createSignal, Show } from "solid-js";
 import { RenderObj } from "./components/RenderObj";
+import JSONWorker from "./lib/JSONWorker?worker";
 
 function App() {
   let [object, setObject] = createSignal(null as any);
   let [error, setError] = createSignal(false);
   let [fileName, setFileName] = createSignal("");
 
-  const w = new Worker("../Worker.js");
+  let w: Worker;
 
   async function handleUploadFile(e: { target: any }) {
     const file = e?.target?.files[0];
     setFileName(file.name);
-    w.postMessage(file);
 
-    w.onmessage = (res) => {
-      const arrBuff = res.data;
-      const start = Date.now();
-      let json = null;
-      try {
-        json = JSON.parse(new TextDecoder().decode(arrBuff as ArrayBuffer));
-        setError(false);
-      } catch (err) {
-        if (err instanceof SyntaxError) {
-          console.log("Invalid JSON file");
-          setError(true);
-        }  
+    if (window.Worker) {
+      if (!(w instanceof Worker)) {
+        w = new JSONWorker();
+
+        w.postMessage(file);
+
+        w.onmessage = (res) => {
+          const arrBuff = res.data;
+          const start = Date.now();
+          let json = null;
+          try {
+            json = JSON.parse(new TextDecoder().decode(arrBuff as ArrayBuffer));
+            setError(false);
+          } catch (err) {
+            if (err instanceof SyntaxError) {
+              console.log("Invalid JSON file");
+              setError(true);
+            }
+          }
+          console.log(`Parse JSON: ${Date.now() - start}ms`);
+          // console.log(json);
+          setObject(json);
+        };
       }
-      console.log(`Parse JSON: ${Date.now() - start}ms`);
-      // console.log(json);
-      setObject(json);
-    };
+    }
   }
-
-  onCleanup(() => {
-    w.terminate();
-  });
 
   return (
     <>
--- src/lib/JSONWorker.ts
+++ src/lib/JSONWorker.ts
@@ -1,18 +1,15 @@
-async function parseJsonFile(file) {
+async function uploadFile(file: Blob) {
     return new Promise((resolve, reject) => {
         const fileReader = new FileReader();
-        fileReader.onloadend = (event) => {
-            const res = event.target.result;
-            return resolve(res);
-        }
+        fileReader.onloadend = (event: any) => resolve(event.target.result);
         fileReader.onerror = (error) => reject(error)
         fileReader.readAsArrayBuffer(file)
     })
 }
 
-onmessage = async (event) => {
+onmessage = async ({data}: MessageEvent<Blob>) => {
     const start = Date.now();
-    const value = await parseJsonFile(event.data);
+    const value = await uploadFile(data);
     console.log(`Worker: ${Date.now() - start}ms`);
     postMessage(value);
 };
