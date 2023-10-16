230dd9cca20bc58f97dac167080f9c140f6af0f5
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2023-10-16 17:31:57 -0300

    feat: implementa um quase lazy loading

--- src/components/RenderObj.tsx
+++ src/components/RenderObj.tsx
@@ -1,12 +1,50 @@
-import { For, Switch, Match, createSignal } from "solid-js";
+import {
+  For,
+  Switch,
+  Match,
+  createSignal,
+  onMount,
+  createEffect,
+} from "solid-js";
+import { createMutable } from "solid-js/store";
+
+const OFFSET = 40;
 
 export const RenderObj = ({ object }: { object: Record<string, any> }) => {
   const [isOpen, setIsOpen] = createSignal(false);
+  const [tam, setTam] = createSignal(OFFSET);
+  const [objSliced, setObjSliced] = createSignal(
+    Object.keys(object).slice(0, OFFSET)
+  );
+  const [target, setTarget] = createSignal(
+    document.querySelector(".container")!
+  );
+  let observer: IntersectionObserver;
+
+  onMount(() => {
+    observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log(entry)
+          setObjSliced(
+            createMutable(Object.keys(object).slice(0, tam() + OFFSET))
+          );
+          setTam((tam) => tam + OFFSET);
+          const el = document.querySelector(".node.last");
+          if (el !== null) setTarget(el);
+        }
+      });
+    });
+  });
+
+  createEffect(() => {
+    observer.observe(target());
+  });
 
   return (
-    <For each={Object.keys(object)}>
-      {(key) => (
-        <div class="node">
+    <For each={objSliced()}>
+      {(key, index) => (
+        <div class="node" classList={{last:index() === objSliced().length - 1}}>
           <Switch>
             <Match
               when={typeof object[key] === "object" && object[key] !== null}
